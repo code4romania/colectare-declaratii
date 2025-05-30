28ceefcaf10c819d36de88a32610f1664f8d9951
Andrei Ioniță <hi@andrei.io>
2025-05-30 23:04:42 +0100

    wip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-use Sentry\Laravel\Integration;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies('*')
+        $middleware->trustProxies('*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         Integration::handles($exceptions);
