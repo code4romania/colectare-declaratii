24dffc1f4a9e973b94612795df9843003f811af1
Lupu Gheorghe <46172059+gheorghelupu17@users.noreply.github.com>
2025-06-18 15:42:49 +0300

    add processing timestamps to declarations (#3)
    * add migrations to  documents
    
    * fix cr
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Andrei Ioniță <hi@andrei.io>

---
+++ database/migrations/2025_06_17_134039_add-coloumns-on-declarations.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('declarations', function (Blueprint $table) {
+            $table->timestamp('started_processing_at')->nullable();
+            $table->timestamp('finished_processing_at')->nullable();
+        });
+    }
+};
