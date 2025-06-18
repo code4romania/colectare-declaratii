772b1e4d486a206bc18b17d9a34989637c35adb6
Lupu Gheorghe <gheorghelupu17@gmail.com>
2025-06-18 13:01:38 +0300

    fix cr

--- database/migrations/2025_06_17_134039_add-coloumns-on-declarations.php
+++ database/migrations/2025_06_17_134039_add-coloumns-on-declarations.php
@@ -12,10 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('declarations', function (Blueprint $table) {
-            $table->dateTime('started_processing_at')->nullable()->after('ip_address');
-            $table->dateTime('finished_processing_at')->nullable()->after('started_processing_at');
-            $table->dateTime('started_validation_at')->nullable()->after('started_processing_at');
-            $table->dateTime('finished_validation_at')->nullable()->after('started_validation_at');
+            $table->timestamp('started_processing_at')->nullable()->after('ip_address');
+            $table->timestamp('finished_processing_at')->nullable()->after('started_processing_at');
 
         });
     }
@@ -25,13 +23,5 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('declarations', function (Blueprint $table) {
-            $table->dropColumn([
-                'started_processing_at',
-                'finished_processing_at',
-                'started_validation_at',
-                'finished_validation_at',
-            ]);
-        });
     }
 };
